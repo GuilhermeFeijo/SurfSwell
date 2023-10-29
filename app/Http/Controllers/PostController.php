b8104c60bc9f9a6639285c10eaf1cd5be4bfecf3
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2023-10-28 21:49:35 -0300

    feat: adicionado paginacao ao index e organizado as rotas

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::get();
+        $posts = Post::orderBy('id')->paginate();
 
         //dd($posts); // usar dd para debugar
 
